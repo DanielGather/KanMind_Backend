5d3cfcc5033205cefa401b5f4cecd007d6e4caaa
Daniel Gather <163533297+DanielGather@users.noreply.github.com>
2025-10-07 19:51:53 +0200

    changed RegistrationView and RegistrationSerializer

--- auth_app/api/serializers.py
+++ auth_app/api/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-
+from django.contrib.auth import authenticate
 class RegistrationSerializer(serializers.ModelSerializer):
     repeated_password = serializers.CharField(write_only=True)
-
+    # username = serializers.CharField(read_only=True)
     class Meta:
         model = User
         fields = ['username', 'email', 'password', 'repeated_password']
@@ -11,9 +11,15 @@ class RegistrationSerializer(serializers.ModelSerializer):
             'password': {'write_only': True}
         }
 
+    def validate_username(self, data):
+        if User.objects.filter(username=data).exists():
+            raise serializers.ValidationError('Username already exists')
+        return data
+
     def save(self, **kwargs):
         pw = self.validated_data['password']
         repeated_pw = self.validated_data['repeated_password']
+
 
         if pw != repeated_pw:
             raise serializers.ValidationError({'error' : 'password dont match'})
@@ -28,3 +34,44 @@ class RegistrationSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError('Email already exists')
         return value
     
+
+class LoginSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+    class Meta:
+        model = User
+        fields = ['email', 'password']
+    def validate(self, data):
+        email = data.get('email')
+        password = data.get('password')
+
+        if email and password:
+            user = authenticate(request=self.context.get('request'),
+                                email=email, password=password)
+            
+            if not user:
+                raise serializers.ValidationError('Ungültige Anmeldedaten', code='authorization')
+        else:
+            raise serializers.ValidationError('E-Mail und Passwort sind erforderlich.', code='authorization')
+
+        data['user'] = user
+        return data
+
+
+    # def validate_email(self, value):
+    #     if User.objects.filter(email=value).exists():
+    #         return value
+    #     raise serializers.ValidationError('Email not exists')
+        
+
+    # def validate_password(self, value):
+    #     if User.objects.filter(password=value).exists():
+    #         return value
+    #     raise serializers.ValidationError('Password not exists')
+
+    # def validate(self, attrs):
+    #     user = User.objects.filter(email=attrs['email'], password=attrs['password']).exists()
+    #     print(user)
+    #     if user:
+    #         return attrs
+    #     raise serializers.ValidationError('User not found')
--- auth_app/api/views.py
+++ auth_app/api/views.py
@@ -3,20 +3,36 @@ from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import AllowAny
-from .serializers import RegistrationSerializer
+from rest_framework import status
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth import get_user_model
+from .serializers import RegistrationSerializer, LoginSerializer
+
+
+User = get_user_model()
 
 class RegistrationView(APIView):
     permission_classes = [AllowAny]
     authentication_classes = []
 
     def post(self, request):
+        fullname = request.data["fullname"].split()
+        fullname = "_".join(fullname)
+        new_data = {
+            "username" : fullname,
+            "email" : request.data["email"],
+            "password" : request.data["password"],
+            "repeated_password" : request.data["repeated_password"]
+        }
         
-        serializer = RegistrationSerializer(data=request.data)
-
+        serializer = RegistrationSerializer(data=new_data)
+        
+        
         data = {}
         if serializer.is_valid():
             saved_account = serializer.save()
             token, created = Token.objects.get_or_create(user=saved_account)
+            saved_account.username = request.data["fullname"]
             data = {
                 'token' : token.key,
                 'fullname' : saved_account.username,
@@ -32,10 +48,21 @@ class RegistrationView(APIView):
 class LoginView(ObtainAuthToken):
     permission_classes = [AllowAny]
     authentication_classes = []
-
     def post(self, request):
-        
+        email = request.data.get("email")
+        if not email:
+            return Response(
+                {"error": "E-Mail ist erforderlich."},
+                status=status.HTTP_400_BAD_REQUEST
+                )
+        user = User.objects.get(email=email)
+            
+        username = user.username
+        print(username)
+
+        request.data["username"] = username
         serializer = self.serializer_class(data=request.data)
+
 
         data = {}
         if serializer.is_valid():
