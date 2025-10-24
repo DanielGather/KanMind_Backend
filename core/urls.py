c9b1dedf1417297e6fc6ad22fcd043fd12c86c12
Daniel Gather <163533297+DanielGather@users.noreply.github.com>
2025-10-24 20:47:31 +0200

    ADD: Add new URLs

--- core/urls.py
+++ core/urls.py
@@ -20,5 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('auth_app.api.urls'))
+    path('api/', include('auth_app.api.urls')),
+    path('api/', include('boards_app.api.urls'))
+
 ]
